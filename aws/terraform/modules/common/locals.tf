12f34fc7104ffafb86628a48a3fb824b7297cc30
Kelly Davis <kldavis4@gmail.com>
2021-08-05 14:03:21 -0400

    add missing locals.tf

---
+++ aws/terraform/modules/common/locals.tf
@@ -0,0 +1,20 @@
+locals {
+  aws_regions = {
+    "us-east-1"      = "usea1"
+    "us-east-2"      = "usea2"
+    "us-west-1"      = "uswe1"
+    "us-west-2"      = "uswe2"
+    "us-gov-west-1"  = "ugwe2"
+    "ca-central-1"   = "cace1"
+    "eu-west-1"      = "euwe1"
+    "eu-west-2"      = "euwe2"
+    "eu-central-1"   = "euce1"
+    "ap-southeast-1" = "apse1"
+    "ap-southeast-2" = "apse2"
+    "ap-south-1"     = "apso1"
+    "ap-northeast-1" = "apne1"
+    "ap-northeast-2" = "apne2"
+    "sa-east-1"      = "saea1"
+    "cn-north-1"     = "cnno1"
+  }
+}
